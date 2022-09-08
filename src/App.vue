2014524ba9adfe6bc4422a9d05af99cc4d90faad
Tea Minova <tea.minova11@gmail.com>
2022-09-08 21:04:31 +0200

    added package

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,34 @@
 <script>
 import TheHeader from '@/components/layout/TheHeader';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAJf0ZfgpKdji8DbzIQ7C-yWT_PlxqKRxg",
+  authDomain: "gallery-ad9c8.firebaseapp.com",
+  projectId: "gallery-ad9c8",
+  storageBucket: "gallery-ad9c8.appspot.com",
+  messagingSenderId: "511763274036",
+  appId: "1:511763274036:web:0442d980b1ed2fdba82d61",
+  measurementId: "G-7D2CYDWLJZ"
+};
+
+
 export default {
   name: 'App',
   components: {
     TheHeader
+  },
+  created() {
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
   }
 };
 </script>
